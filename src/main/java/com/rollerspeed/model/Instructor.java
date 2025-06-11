a3aaf04402142f50b150df791d369c9e5cb2388e
iu peliculas <robinson.marin@est.iudigital.edu.co>
2025-06-10 23:20:48 -0500

    actualizacion4.0

--- src/main/java/com/rollerspeed/model/Instructor.java
+++ src/main/java/com/rollerspeed/model/Instructor.java
@@ -7,62 +7,10 @@ import javax.persistence.Id;
 
 @Entity
 public class Instructor {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String email;
     private String telefono;
-    private String especialidad; // Especialidad del instructor
-    private String fechaInscripcion; // Fecha de inscripción
-
-    // Getters y Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getEspecialidad() {
-        return especialidad;
-    }
-
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
-    }
-
-    public String getFechaInscripcion() {
-        return fechaInscripcion;
-    }
-
-    public void setFechaInscripcion(String fechaInscripcion) {
-        this.fechaInscripcion = fechaInscripcion;
-    }
 }
